80ff4210c82267c5c415f2e2456d13c93117014f
ashik081 <freelancerashik81@gmail.com>
2026-04-01 09:23:31 +0600

    same issuses fixed

--- src/Components/StateSection.jsx
+++ src/Components/StateSection.jsx
@@ -5,20 +5,20 @@ const StateSection = () => {
     <div className="w-full bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white">
       <div className="container mx-auto flex justify-center">
         <div className="stats stats-vertical lg:stats-horizontal shadow w-full overflow-x-hidden">
-          <div className="stat place-items-center space-y-2 m-8 lg:m-15  lg:border-r-2 lg:border-r-gray-300">
-            <div className="stat-value text-6xl">50K+</div>
+          <div className="stat place-items-center space-y-2 my-8 lg:my-15  lg:border-r-2 lg:border-r-gray-300">
+            <div className="stat-value text-5xl">50K+</div>
             <div className="stat-desc text-2xl text-gray-300">Active Users</div>
           </div>
 
-          <div className="stat place-items-center space-y-2 m-8 lg:m-15 border-r-2 border-r-gray-300">
-            <div className="stat-value text-6xl">200K+</div>
+          <div className="stat place-items-center space-y-2 my-8 lg:my-15 border-r-2 border-r-gray-300">
+            <div className="stat-value text-5xl">200K+</div>
             <div className="stat-desc text-2xl text-gray-300">
               Premium Tools
             </div>
           </div>
 
-          <div className="stat place-items-center space-y-2 m-8 lg:m-15">
-            <div className="stat-value text-6xl">4.9</div>
+          <div className="stat place-items-center space-y-2 my-8 lg:my-15">
+            <div className="stat-value text-5xl">4.9</div>
             <div className="stat-desc text-2xl text-gray-300">Rating</div>
           </div>
         </div>
